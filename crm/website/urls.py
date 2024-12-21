1bc5861b4c399cf9830fc83f75a8b4f44129a537
mustafoyev-202 <baxtiyormustafoyev2006@gmail.com>
2024-12-21 23:08:19 +0500

    first commit

--- crm/website/urls.py
+++ crm/website/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.home,name="home"),
-    path("login", views.login_user, name="login"),
-    path("logout", views.logout_user, name="logout"),
-
+    path("", views.home, name="home"),             # Home/login page
+    path("logout", views.logout_user, name="logout"),  # Logout page
 ]
--- crm/website/views.py
+++ crm/website/views.py
@@ -1,26 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
-# Create your views here.
 
+# Home view
 def home(request):
-    return render(request, "home.html", {})
-
-def login_user(request):
     if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
+        # Get username and password from POST request
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        # Authenticate the user
         user = authenticate(username=username, password=password)
+
         if user is not None:
+            # Login the user
             login(request, user)
             messages.success(request, "You have successfully logged in")
             return redirect("home")
         else:
-            messages.error(request, "Invalid username or password")
-            return redirect("login")
-    return render(request, "login.html", {})
+            # Authentication failed
+            messages.warning(request, "Invalid username or password")
+            return redirect("home")
+    else:
+        # Render the home page
+        return render(request, "home.html", {})
 
+
+# Logout view
 def logout_user(request):
     logout(request)
     messages.success(request, "You have successfully logged out")
