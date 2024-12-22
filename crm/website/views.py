7aa76d2629f05101b70827be5c90dff2f5543a80
mustafoyev-202 <baxtiyormustafoyev2006@gmail.com>
2024-12-22 20:30:37 +0500

    first commit

--- crm/website/views.py
+++ crm/website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 
 # Home view
@@ -35,4 +36,17 @@ def logout_user(request):
 
 
 def register_user(request):
-    return render(request, "register.html", {})
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password1")
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, f"Account was created for {username}")
+            return redirect("home")
+    else:
+        form = SignUpForm()
+        return render(request, "register.html", {"form": form})
+    return render(request, "register.html", {"form": form})
